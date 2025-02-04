57bb8b3475f1230aeceaff2b6ce9f319abd475f8
selva-mern12 <selva.mern12@gmail.com>
2025-02-04 23:04:48 +0000

    enhacment simple todo

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -6,64 +6,177 @@ const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
+    completed: false,
   },
   {
     id: 2,
     title: 'Rent the movie for tomorrow movie night',
+    completed: false,
   },
   {
     id: 3,
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    completed: false,
   },
   {
     id: 4,
     title: 'Drop the parcel at Bloomingdale',
+    completed: false,
   },
   {
     id: 5,
     title: 'Order fruits on Big Basket',
+    completed: false,
   },
   {
     id: 6,
     title: 'Fix the production issue',
+    completed: false,
   },
   {
     id: 7,
     title: 'Confirm my slot for Saturday Night',
+    completed: false,
   },
   {
     id: 8,
     title: 'Get essentials for Sunday car wash',
+    completed: false,
   },
 ]
 
 // Write your code here deleteTodo
 
 class SimpleTodos extends Component {
-  state = {userTodoList: initialTodosList}
+  state = {
+    userTodoList: [],
+    addTodo: false,
+    userTodo: '',
+    showMsg: true,
+  }
+
+  componentDidMount() {
+    const savedTodo = localStorage.getItem('todo-list')
+    if (!savedTodo) {
+      localStorage.setItem('todo-list', JSON.stringify(initialTodosList))
+      this.setState({userTodoList: initialTodosList})
+    } else {
+      this.setState({userTodoList: JSON.parse(savedTodo)})
+    }
+    setTimeout(() => this.setState({showMsg: false}), 5000)
+  }
+
+  componentDidUpdate(prevState) {
+    const {userTodoList} = this.state
+    if (userTodoList !== prevState.userTodoList) {
+      localStorage.setItem('todo-list', JSON.stringify(userTodoList))
+    }
+  }
 
   deleteTodo = id => {
     const {userTodoList} = this.state
     const filterTodo = userTodoList.filter(eachTodo => eachTodo.id !== id)
-    this.setState({userTodoList: filterTodo})
+    this.setState({
+      userTodoList: filterTodo.map(todo =>
+        todo.id > id ? {...todo, id: todo.id - 1} : todo,
+      ),
+    })
     console.log(id)
   }
 
+  editTodo = (id, userText) => {
+    this.setState(prevState => ({
+      userTodoList: prevState.userTodoList.map(item =>
+        item.id === id ? {...item, title: userText} : item,
+      ),
+    }))
+  }
+
+  saveTodo = () => {
+    const {userTodo, userTodoList} = this.state
+    if (userTodo !== '') {
+      const id = userTodoList.length + 1
+      const title = userTodo
+      this.setState(prevState => ({
+        userTodoList: [
+          ...prevState.userTodoList,
+          {id, title, completed: false},
+        ],
+        addTodo: false,
+        userTodo: '',
+      }))
+    }
+  }
+
+  completeTase = id =>
+    this.setState(prevState => ({
+      userTodoList: prevState.userTodoList.map(todo =>
+        todo.id === id ? {...todo, completed: !todo.completed} : todo,
+      ),
+    }))
+
   render() {
-    const {userTodoList} = this.state
+    const {userTodoList, addTodo, userTodo, showMsg} = this.state
+    // userTodoList.length > 0 && userTodoList.sort((a, b) => b.id - a.id)
+
     return (
       <div className="bg-container">
-        <div className="main-container">
-          <h1>Simple Todos</h1>
-          <ul>
-            {userTodoList.map(todo => (
-              <TodoItem
-                todo={todo}
-                key={todo.id}
-                deleteTodo={this.deleteTodo}
-              />
-            ))}
-          </ul>
+        <div className="todo-bg">
+          <div className="main-container">
+            <div className="heading">
+              <p className="empty"> </p>
+              <h1>Simple Todos</h1>
+              <div className="add-todo-container">
+                {addTodo ? (
+                  <>
+                    <input
+                      className="add-todo"
+                      type="text"
+                      value={userTodo}
+                      placeholder="Add your Task..."
+                      onChange={event =>
+                        this.setState({userTodo: event.target.value})
+                      }
+                    />
+
+                    <button
+                      type="button"
+                      className="add-todo-button save-todo"
+                      onClick={this.saveTodo}
+                    >
+                      Save Todo
+                    </button>
+                  </>
+                ) : (
+                  <div style={{display: 'flex', alignItems: 'center'}}>
+                    {showMsg && (
+                      <p className="intro-msg">
+                        (Sample Todos here), Add to your task
+                      </p>
+                    )}
+                    <button
+                      type="button"
+                      className="add-todo-button"
+                      onClick={() => this.setState({addTodo: true})}
+                    >
+                      Add Todo
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+            <ul className="todo-list">
+              {userTodoList?.map(todo => (
+                <TodoItem
+                  todo={todo}
+                  key={todo.id}
+                  deleteTodo={this.deleteTodo}
+                  editTodo={this.editTodo}
+                  completeTase={this.completeTase}
+                />
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     )
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,17 +1,66 @@
+import {useState} from 'react'
 import './index.css'
 
 const TodoItem = props => {
-  const {todo, deleteTodo} = props
-  const {title, id} = todo
-  const onDelete = () => {
-    deleteTodo(id)
+  const {todo, deleteTodo, editTodo, completeTase} = props
+  const {title, id, completed} = todo
+  const [todoEdit, setTodoEdit] = useState(false)
+  const [userText, setUserEdit] = useState(title)
+  const todoSave = () => {
+    setTodoEdit(prevTodo => !prevTodo)
+    editTodo(id, userText)
   }
   return (
     <li>
-      <p>{title}</p>
-      <button type="button" onClick={onDelete}>
-        Delete
-      </button>
+      {!todoEdit ? (
+        <div className="checkbox-task">
+          <input
+            type="checkbox"
+            id={`checkbox-${id}`}
+            checked={completed}
+            onChange={() => completeTase(id)}
+          />
+          <label
+            htmlFor={`checkbox-${id}`}
+            className={completed ? 'complete' : ''}
+          >
+            <p>{title}</p>
+          </label>
+        </div>
+      ) : (
+        <input
+          className="user-input"
+          type="text"
+          value={userText}
+          onChange={event => setUserEdit(event.target.value)}
+        />
+      )}
+      <div className="todo-buttons">
+        {!todoEdit ? (
+          <button
+            type="button"
+            onClick={() => setTodoEdit(prevTodo => !prevTodo)}
+            className="delete-button edit-button"
+          >
+            Edit
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={todoSave}
+            className="delete-button edit-button"
+          >
+            Save
+          </button>
+        )}
+        <button
+          type="button"
+          onClick={() => deleteTodo(id)}
+          className="delete-button"
+        >
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
